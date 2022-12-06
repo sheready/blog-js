0e48f7e66d74f593c62e747c551ea5e8be3f505a
sheready <csstlw@gmail.com>
2022-12-06 23:36:52 +0300

    add styling to class card

--- index.js
+++ index.js
@@ -4,31 +4,52 @@ fetch("http://localhost:3000/articles")
 .then(response => response.json())
 .then(data => data.forEach(data => fetchArticles(data)))
 
+
+
 //CRUD -> Create, Read, Update and Delete
 
 function fetchArticles(data){
     let articleBody = document.querySelector('.card-body');
     let articleContainer = document.createElement('div');
+    let articleDetails = document.createElement('div');
+    articleContainer.className = "articles";
 
     articleContainer.innerHTML = `
         <img src="${data.image}" style="width:250px;height:200px">
-        <h3>${data.title}</h3>
-        <p>${data.content}</p>
+       
     `
     articleBody.appendChild(articleContainer);
-    articleContainer.style.display = "flex";
-    articleContainer.style.flexDirection = "row";
-    articleContainer.style.flexWrap = "wrap";
-    articleContainer.style.marginLeft = "5px";
+    articleContainer.appendChild(articleDetails);
+    articleContainer.style.height = "280px";
+    
+
+    
+
+    articleContainer.addEventListener('click', () => {
+        fetch(`http://localhost:3000/articles/${data.id}`) 
+        .then(response => response.json())
+        .then(data => {
+            articleDetails.innerHTML = `
+            <h3>${data.title}</h3>
+            <p>${data.content}</p>  
+        
+        `
+        })
+    })
+   
 
 }
 fetchArticles();
 
+
 function createArticle(){
     let articleBody = document.querySelector('.card-body');
+    let articleContainer = document.createElement('div');
     let articleForm = document.querySelector('#form-container');
+    articleContainer.className = "articles";
     articleForm.addEventListener('submit', (e) => {
-        e.preventDefault();
+    e.preventDefault();
+
         console.log(e.target);
         const titleInput = articleForm.querySelector('#title').value
         const contentInput = articleForm.querySelector('#content').value
@@ -46,12 +67,17 @@ function createArticle(){
             }
         }).then(response => response.json())
           .then(data => {
-            articleBody.innerHTML += `
+            articleContainer.innerHTML = `
                 <img src="${data.image}" style="width:250px;height:200px">
                 <h3>${data.title}</h3>
                 <p>${data.content}</p>  
             
             `
+            articleBody.appendChild(articleContainer);
+            articleContainer.style.display = "flex";
+            articleContainer.style.flexDirection = "row";
+            articleContainer.style.flexWrap = "wrap";
+            articleContainer.style.marginLeft = "5px";
           })
     })
 }
